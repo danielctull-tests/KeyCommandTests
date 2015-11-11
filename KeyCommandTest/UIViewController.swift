a36754852311a2ab586addd10fc3497563ddf7e7
Daniel Tull <dt@danieltull.co.uk>
2015-11-11 22:10:18 +0000

    Provide special cases for standard container view controllers

--- KeyCommandTest/UIViewController.swift
+++ KeyCommandTest/UIViewController.swift
@@ -12,3 +12,50 @@ extension UIViewController {
 		}
 	}
 }
+
+// For special view controllers, we only want to go through the
+// on screen view controllers.
+
+extension UINavigationController {
+
+	override func recurseChildViewControllers(block: UIViewController -> ()) {
+		block(self)
+		topViewController?.recurseChildViewControllers(block)
+	}
+}
+
+extension UITabBarController {
+
+	override func recurseChildViewControllers(block: UIViewController -> ()) {
+		block(self)
+		selectedViewController?.recurseChildViewControllers(block)
+	}
+}
+
+extension UISplitViewController {
+
+	override func recurseChildViewControllers(block: UIViewController -> ()) {
+
+		block(self)
+
+		guard !collapsed || viewControllers.count == 2 else {
+			super.recurseChildViewControllers(block)
+			return
+		}
+
+		switch displayMode {
+
+			case .AllVisible:
+				super.recurseChildViewControllers(block)
+
+			case .PrimaryHidden:
+				viewControllers[1].recurseChildViewControllers(block)
+
+			case .PrimaryOverlay:
+				viewControllers[0].recurseChildViewControllers(block)
+
+			// Never going to happen
+			case .Automatic: break
+		}
+	}
+}
